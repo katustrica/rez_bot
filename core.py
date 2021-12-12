4613599a2326a4bd3b9f2f4e0bd338c7b42317c2
ma.minnahmetov <gtr425gtr@gmail.com>
2021-12-12 14:21:38 +0300

    Русский

--- core.py
+++ core.py
@@ -2,9 +2,9 @@ from enum import Enum, IntEnum
 import json
 
 class HourType(IntEnum):
-    FREE = 0
-    BUSY = 1
-    RELAX = 2
+    СВОБОДНОЕ = 0
+    ЗАНЯТОЕ = 1
+    ОТДЫХ = 2
 
 
 week_info = dict[str, dict[str, int]]
@@ -34,15 +34,15 @@ class Rezeda():
 
     def get_info(self) -> str:
         week = self.get_week_info()
-        free_time = self.get_time(HourType.FREE, week)
-        relax_time = self.get_time(HourType.RELAX, week)
-        return '\n\n'.join([free_time, relax_time])
+        free_time = self.get_time(HourType.СВОБОДНОЕ, week)
+        # relax_time = self.get_time(HourType.ОТДЫХ, week)
+        return '\n\n'.join([free_time])
 
     def get_week_info(self) -> week_info:
         """
         Вернуть календарь с часами по дням. 1 - занято, 2 - отдых, 0 - свободно.
         """
-        with open(self.__file_name, 'r') as week_json:
+        with open(self.__file_name, 'r', encoding='utf8') as week_json:
             return json.load(week_json)
 
     def update_week_info(self, day, hour, type) -> None:
@@ -51,11 +51,11 @@ class Rezeda():
         """
         week = self.get_week_info()
         week[day][hour] = int(type)
-        with open(self.__file_name, 'w') as week_json:
+        with open(self.__file_name, 'w', encoding='utf8') as week_json:
             return json.dump(week, week_json, ensure_ascii=False, indent=4)
 
     def get_time(self, time_type: HourType, week: week_info) -> str:
-        header_str = f'--------------\n{time_type.name.title()} time\n--------------\n'
+        header_str = f'--------------\n{time_type.name.title()} время\n--------------\n'
         for day, hours_dict in week.items():
             hours = [hour for hour, hour_type in hours_dict.items() if hour_type == time_type]
             if hours:
